ae039585855eff9a79f4fb9cc61c92f82200f40a
bushido <nakarasido5@gmail.com>
2024-01-10 21:49:40 +0700

    add rabbitmq config and producer

---
+++ src/main/java/com/example/springbootrabbitmq/publisher/RabbitMQProducer.java
@@ -0,0 +1,39 @@
+package com.example.springbootrabbitmq.publisher;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+@Service
+
+public class RabbitMQProducer {
+
+    private static final Logger logger = LoggerFactory.getLogger(RabbitMQProducer.class);
+
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    /*
+    *Using RabbitTemplate to Send Messages
+    *in Springboot, RabbitTemplate automatic configure.
+    *Just need to inject and use it
+    */
+
+    private RabbitTemplate rabbitTemplate;
+
+    public RabbitMQProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String message) {
+        logger.info(String.format("Message sent -> %s" , message));
+        rabbitTemplate.convertAndSend(exchange,routingKey, message);
+    }
+}
